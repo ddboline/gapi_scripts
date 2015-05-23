fd596757b444d52045481267312a9e6501f1f449
Daniel Boline <daniel.boline@stonybrook.edu>
2015-05-23 17:54:54 -0400

    ...

--- setup_cloud9.sh
+++ setup_cloud9.sh
@@ -3,4 +3,5 @@
 sudo apt-get install -y python-googleapi python-tz python-dateutil
 # sudo apt-get install -y libroot-core-dev libroot-math-physics-dev libroot-graf2d-postscript-dev libroot-bindings-python-dev
 # sudo apt-get install -y x11vnc novnc fluxbox vnc4server
+scp ddboline@ddbolineathome.mooo.com:~/setup_files/build/ddboline_personal_scripts/client_secrets.json .
 scp ddboline@ddbolineathome.mooo.com:~/setup_files/build/ddboline_personal_scripts/*.dat .

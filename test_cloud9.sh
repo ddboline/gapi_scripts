9a27b5e4b5be450a4da185d0410c2a2043988bf7
Daniel Boline <ddboline@gmail.com>
2015-08-12 20:42:59 -0400

    remove parse_glirc from tests

--- test_cloud9.sh
+++ test_cloud9.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 ./parse_physics.py pcal week
-./parse_glirc.py pcal week
 ./parse_nycruns.py pcal week
 ./print_todays_agenda.py
 ./list_drive_files.py list 10
--- test_conda.sh
+++ test_conda.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 python3 ./parse_physics.py pcal week
-python3 ./parse_glirc.py pcal week
 python3 ./parse_nycruns.py pcal week
 python3 ./print_todays_agenda.py
 python3 ./list_drive_files.py list 10

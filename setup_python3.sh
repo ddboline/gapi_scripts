7874c1b22bf61399cfe1216f3adf227c012f10ac
Daniel Boline <ddboline@gmail.com>
2015-08-18 18:37:18 -0400

    ...

---
+++ setup_python3.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+### hack...
+export LANG="C.UTF-8"
+
+sudo bash -c "echo deb ssh://ddboline@ddbolineathome.mooo.com/var/www/html/deb/trusty/python3/devel ./ > /etc/apt/sources.list.d/py2deb2.list"
+sudo apt-get update
+sudo apt-get install -y python3-google-api-python-client python3-tz python3-dateutil python3-requests
+# sudo apt-get install -y python3-googleapi python3-tz python3-dateutil
+scp ddboline@ddbolineathome.mooo.com:~/setup_files/build/ddboline_personal_scripts/client_secrets.json .
+scp ddboline@ddbolineathome.mooo.com:~/setup_files/build/ddboline_personal_scripts/*.dat .

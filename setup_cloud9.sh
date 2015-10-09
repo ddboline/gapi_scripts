1fb2de33c9bbc7dcab0e7cf0a45ad09f23191f97
Daniel Boline <ddboline@gmail.com>
2015-10-09 19:40:49 -0400

    ...

--- setup_cloud9.sh
+++ setup_cloud9.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-sudo apt-get install -y python-googleapi python-tz python-dateutil python-requests python-nose
-# sudo apt-get install -y python3-googleapi python3-tz python3-dateutil
+sudo apt-get install -y python-googleapi python-tz python-dateutil \
+                        python-requests python-nose python-coverage 
+
 scp ddboline@ddbolineathome.mooo.com:~/setup_files/build/ddboline_personal_scripts/client_secrets.json .
 scp ddboline@ddbolineathome.mooo.com:~/setup_files/build/ddboline_personal_scripts/*.dat .

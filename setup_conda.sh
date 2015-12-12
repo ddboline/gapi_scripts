7f73793a798c52e5e69128bfd2bab20e237b2d61
Daniel Boline <ddboline@gmail.com>
2015-12-11 21:09:37 -0500

    ...

--- setup_conda.sh
+++ setup_conda.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo /opt/conda/bin/conda install -c https://conda.anaconda.org/ddboline --yes pytz dateutil google-api-python-client nose
+sudo /opt/conda/bin/conda install -c https://conda.anaconda.org/ddboline --yes pytz python-dateutil google-api-python-client nose
 
 # sudo apt-get install -y python-googleapi python-tz python-dateutil
 # sudo apt-get install -y python3-googleapi python3-tz python3-dateutil
@@ -8,4 +8,4 @@ scp ddboline@ddbolineathome.mooo.com:~/setup_files/build/ddboline_personal_scrip
 scp ddboline@ddbolineathome.mooo.com:~/setup_files/build/ddboline_personal_scripts/*.dat .
 
 ### stupid stupid hack...
-sudo chmod a+r /opt/conda/lib/python3.4/site-packages/httplib2/cacerts.txt
+sudo chmod a+r /opt/conda/lib/python3*/site-packages/httplib2/cacerts.txt

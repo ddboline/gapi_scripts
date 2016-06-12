a276f75ab8b1a5365dec0f2d7a936a03e450198b
Daniel Boline <ddboline@gmail.com>
2016-06-11 21:26:25 -0400

    ...

--- setup_cloud9.sh
+++ setup_cloud9.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-sudo apt-get install -y python-googleapi python-tz python-dateutil \
-                        python-requests python-nose python-coverage \
-                        python-setuptools python-dev python-bs4
+export LANG="C.UTF-8"
+
+sudo bash -c "echo deb ssh://ddboline@ddbolineathome.mooo.com/var/www/html/deb/trusty/devel ./ > /etc/apt/sources.list.d/py2deb.list"
+sudo apt-get update
+sudo apt-get install -y --force-yes python-googleapi python-tz python-dateutil \
+                                    python-requests python-nose python-coverage \
+                                    python-setuptools python-dev python-bs4
 
 scp ddboline@ddbolineathome.mooo.com:~/setup_files/build/ddboline_personal_scripts/client_secrets.json .
 scp ddboline@ddbolineathome.mooo.com:~/setup_files/build/ddboline_personal_scripts/*.dat .

3962c514062147a8d365a9376a2c6232ce97843c
Daniel Boline <ddboline@gmail.com>
2016-12-04 18:51:09 -0500

    ...

--- setup_cloud9.sh
+++ setup_cloud9.sh
@@ -2,7 +2,7 @@
 
 export LANG="C.UTF-8"
 
-sudo bash -c "echo deb ssh://ddboline@ddbolineathome.mooo.com/var/www/html/deb/trusty/devel ./ > /etc/apt/sources.list.d/py2deb.list"
+sudo bash -c "echo deb ssh://ddboline@ddbolineathome.mooo.com/var/www/html/deb/xenial/devel ./ > /etc/apt/sources.list.d/py2deb.list"
 sudo apt-get update
 sudo apt-get install -y --force-yes python-googleapi python-tz python-dateutil \
                                     python-requests python-nose python-coverage \

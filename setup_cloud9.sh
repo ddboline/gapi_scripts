21d6336670f04259a986c174e0ea76ece80e05dd
Daniel Boline <ddboline@gmail.com>
2017-01-01 05:48:18 -0600

    ...

--- setup_cloud9.sh
+++ setup_cloud9.sh
@@ -2,11 +2,17 @@
 
 export LANG="C.UTF-8"
 
-sudo bash -c "echo deb ssh://ddboline@ddbolineathome.mooo.com/var/www/html/deb/xenial/devel ./ > /etc/apt/sources.list.d/py2deb.list"
+#USERNAME=ddboline
+#HOST=ddbolineathome.mooo.com
+USERNAME=ubuntu
+HOST=ddbolineinthecloud.mooo.com
+
+sudo bash -c "echo deb ssh://${USERNAME}@${HOST}/var/www/html/deb/xenial/devel ./ > /etc/apt/sources.list.d/py2deb.list"
+
 sudo apt-get update
 sudo apt-get install -y --force-yes python-googleapi python-tz python-dateutil \
                                     python-requests python-nose python-coverage \
                                     python-setuptools python-dev python-bs4
 
-scp ddboline@ddbolineathome.mooo.com:~/setup_files/build/ddboline_personal_scripts/client_secrets.json .
-scp ddboline@ddbolineathome.mooo.com:~/setup_files/build/ddboline_personal_scripts/*.dat .
+scp ${USERNAME}@${HOST}:~/setup_files/build/ddboline_personal_scripts/client_secrets.json .
+scp ${USERNAME}@${HOST}:~/setup_files/build/ddboline_personal_scripts/*.dat .

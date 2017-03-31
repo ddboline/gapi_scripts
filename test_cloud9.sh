6055ddfaaf4a55f7335f2929de3971589045bb8a
Daniel Boline <ddboline@gmail.com>
2017-03-31 07:34:35 -0400

    ...

--- test_cloud9.sh
+++ test_cloud9.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-py.test `find . -iname '*.py'`
+py.test `find . -iname '*.py' ! -path './send_to_gtalk.py' ! -path './hangups_common.py'`
 ./parse_physics.py pcal week
 ./parse_nycruns.py pcal week
 ./parse_hashnyc.py pcal week
